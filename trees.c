01b5405680e455f54f464ce227b4b2b681ce8d62
ADITYA C <adi2tya98@gmail.com>
2020-11-05 19:31:11 +0530

    Successfully addedd the first binary tree file

---
+++ trees.c
@@ -0,0 +1,69 @@
+#define int_min 0
+#include <stdio.h>
+#include<stdlib.h>
+
+struct nt{
+    struct nt *left;
+    int data;
+    struct nt *right;
+};
+
+void preorder(struct nt *a){
+    if(a){
+    printf("%d ",a->data);
+    preorder(a->left);
+    preorder(a->right);
+}
+}
+void inorder(struct nt *a){
+if(a){
+    inorder(a->left);
+    printf("%d ",a->data);
+    inorder(a->right);
+}
+}
+
+void postorder(struct nt *a){
+if(a){
+    postorder(a->left);
+    postorder(a->right);
+     printf("%d ",a->data);
+}
+}
+
+struct nt* newnode(int a){
+struct nt *n=(struct nt*)malloc(sizeof(struct nt));
+n->data=a;
+n->right=NULL;
+n->left=NULL;
+return(n);
+};
+
+
+
+int main() {
+int i,j,b;
+    printf("***** Traversal of binary tree *****\n");
+      struct nt *a=(struct nt*)malloc(sizeof(struct nt));
+
+
+      a->data=1;
+      a->left=newnode(2);
+      a->right=newnode(3);
+      a->left->right=newnode(5);
+      a->left->left=newnode(4);
+      a->right->left=newnode(6);
+      a->right->right=newnode(7);
+
+printf("PreOrder is\t");
+      preorder(a);
+      printf("\n");
+      printf("InOrder is\t");
+     inorder(a);
+      printf("\n");
+      printf("PostOrder is\t");
+      postorder(a);
+      printf("\n");
+
+    return 0;
+}
