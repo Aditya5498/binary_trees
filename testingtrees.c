1c8af6ea960b04ff40998bf7fbae5ea518a0b65d
ADITYA C <adi2tya98@gmail.com>
2020-11-08 21:29:48 +0530

    Added mirror functionality to binary tree

--- testingtrees.c
+++ testingtrees.c
@@ -32,7 +32,7 @@ else{
 
 void pl(struct node*a){
 int i,j;
-for(i=0;i<4;i++){
+for(i=0;i<3;i++){
     print(a,i);
     printf("\n");
 }
@@ -79,8 +79,27 @@ if(a){
 }
 }
 
+int sum(struct node *a){
+if(a){
+    return sum(a->left)+sum(a->right)+a->data;
+}
+}
+
+struct node *mirror(struct node *a){
+       struct node *temp;
+if(a){
+   mirror(a->left);
+    mirror(a->right);
+    temp = a->left;
+    a->left=a->right;
+    a->right=temp;
+    }
+     return a;
+};
+
 void main(void){
 struct node *a = (struct a*)malloc(sizeof(struct node));
+struct node *temp = (struct a*)malloc(sizeof(struct node));
 int i,j;
 a->data=0;
 a->left=newnode(1);
@@ -89,19 +108,25 @@ a->left->left=newnode(3);
 a->left->right=newnode(4);
 a->right->left=newnode(5);
 a->right->right=newnode(6);
-a->right->right->right=newnode(7);
 
 printf("Counting number of leaf nodes...\n The number of leaf nodes are %d\n",leafs(a));
 printf("\nThe number of half leafs are %d\n",hleafs(a));
 
-printf("The number of elements in the tree are %d\n\n Below is the binary tree \n",max(a));
-pl(a);
+printf("The sum of all the elements is %d\n",sum(a));
+
 
 printf("\n The height of the tree is %d\n\n",height(a));
+printf("The number of elements in the tree are %d\n\n Below is the binary tree \n",max(a));
+pl(a);
+temp=mirror(a);
+printf("\nThe mirrored tree is\n");
+pl(temp);
 
-printf("Deleting the tree\n");
+printf("\nDeleting the tree\n");
 del(a);
-free(a);
-printf("Successfully deleted the tree\n");
 
+printf("\nSuccessfully deleted the tree\n");
+
+printf("\n ********* DOne with all the code ********");
+free(a);
 }
